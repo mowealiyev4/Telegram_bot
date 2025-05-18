d7b4cf0aa15d75b27146bf234ef7fdf1775d0839
mowealiyev4 <mowealiyev556@gmail.com>
2025-05-18 23:52:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Botu başlat
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("Sohbet", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 print("Bot işləyir (insan kimi qısa, reply ilə, zarafatla)...")
